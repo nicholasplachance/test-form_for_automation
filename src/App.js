4312260e7734ee7117f84f92e777c155bc1d1ec4
Nicholas LaChance <nicholasplachance@gmail.com>
2021-08-28 13:42:33 -0500

    Create form for testing automation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form name='contact-form' method='post' data-netlify='true'>
+        <input type='hidden' name='form-name' value='contact-form' />
+        <input type='text' placeholder='Enter your name' name='name' />
+        <input type='email' placeholder='Enter your email' name='email' />
+        <input
+          type='number'
+          placeholder='Enter your phone number'
+          name='phone'
+        />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
