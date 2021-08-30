ae5dbe6d0ef7ea2af2f7a22489d611fd09db3393
Nicholas LaChance <nicholasplachance@gmail.com>
2021-08-30 12:20:22 -0500

    Create styles and make form look better

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className='App'>
-      <form name='contact-form' method='post' data-netlify='true'>
+      <h1>Send us your info!</h1>
+      <form
+        className='form'
+        name='contact-form'
+        method='post'
+        data-netlify='true'>
         <input type='hidden' name='form-name' value='contact-form' />
         <input type='text' placeholder='Enter your name' name='name' />
         <input type='email' placeholder='Enter your email' name='email' />
